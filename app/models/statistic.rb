70ef3da49cac46bf7884d68541a0a1b9f8d6f297
Richard Dale <richard.dale@codethink.co.uk>
2011-10-11 17:23:31 +0100

    * Add a calc_duration() method called before saving a statistics row

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -13,6 +13,19 @@ class Statistic < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
 
+  before_save :calc_duration
+
+  def calc_duration
+    if entry_time && leave_time
+      number_of_days = (self.leave_time - self.entry_time) / (3600 * 24)
+      number_of_days = (number_of_days - 0.5).round
+      week_day_of_leave = self.leave_time.wday
+      self.duration = self.leave_time - self.entry_time
+      self.duration -= ((number_of_days / 7) * 2 * 3600 * 24)
+      self.duration -= (2 * 3600 * 24 ) if (number_of_days % 7) - 2 >= week_day_of_leave
+    end
+  end
+
   # --- Permissions --- #
 
   def create_permitted?
