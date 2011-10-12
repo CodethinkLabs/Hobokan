c6d40c2f6e0529176ff88477c19ac2b8d994b0d3
Richard Dale <richard.dale@codethink.co.uk>
2011-10-12 11:39:18 +0100

    * Customize the application title and project page

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,9 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module CtKanban
   class Application < Rails::Application
-  
+
   config.hobo.dryml_only_templates = true
+  config.hobo.app_name = "Codethink Kanban"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
