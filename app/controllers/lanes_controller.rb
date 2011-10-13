3757ceb53d2180f5218eade0b2818b94260fadc5
Richard Dale <richard.dale@codethink.co.uk>
2011-10-13 11:42:54 +0100

    * Remove redundant new() and create() methods in the lanes controller

--- app/controllers/lanes_controller.rb
+++ app/controllers/lanes_controller.rb
@@ -3,12 +3,8 @@ class LanesController < ApplicationController
   hobo_model_controller
 
   auto_actions :all, :except => :index
+  # auto_actions :all
 
   auto_actions_for :project, [:new, :create]
 
-  def new
-  end
-
-  def create
-  end
 end
