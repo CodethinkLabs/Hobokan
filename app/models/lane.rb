192a8f719740d4469ab9026bc17b337a933e95f4
Richard Dale <richard.dale@codethink.co.uk>
2011-10-14 10:09:54 +0100

    * Add a project scope to the acts_as_list call for a Lane

--- app/models/lane.rb
+++ app/models/lane.rb
@@ -17,7 +17,7 @@ class Lane < ActiveRecord::Base
 
   children :items
 
-  acts_as_list
+  acts_as_list :scope => :project
 
   belongs_to :project
   belongs_to :super_lane, :class_name => 'Lane', :foreign_key => 'super_lane_id'
