5547bf82e6d10a00dfe1969a8ed9b7eabde4544e
Richard Dale <richard.dale@codethink.co.uk>
2011-10-21 09:58:31 +0100

    * Create a custom kanban-board view tag and use it in the item edit form so that the item can be edited under the kanban board

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,36 @@ class ItemsController < ApplicationController
 
   auto_actions :all, :except => :index
   auto_actions_for :lane, :create
+
+  def edit
+    if request.xhr?
+      @item = Item.find(params[:item_id])
+      @old_lane = @item.lane
+      @lane = Lane.find(params[:lane_id])
+      @item.lane = @lane
+      @item.save
+      position = 1
+      params[:item_ordering].each do |id|
+        item = Item.find(id)
+        item.position = position
+        item.save
+        position += 1
+      end
+
+      if @old_lane.id != @lane.id
+        position = 1
+        @old_lane.items.each do |item|
+          item.position = position
+          item.save
+          position += 1
+        end
+      end
+
+      hobo_ajax_response
+      return
+    end
+
+    @item = find_instance
+  end
+
 end
