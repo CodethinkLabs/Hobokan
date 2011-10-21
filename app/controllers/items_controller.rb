426c1ed04e6d7a9ff855a63ce87da81c5e75e5af
Richard Dale <richard.dale@codethink.co.uk>
2011-10-21 13:06:00 +0100

    * Tidy up the Items controller code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,28 +7,7 @@ class ItemsController < ApplicationController
 
   def new_for_lane
     if request.xhr?
-      @item = Item.find(params[:item_id])
-      @old_lane = @item.lane
-      @lane = Lane.find(params[:lane_id])
-      @item.lane = @lane
-      @item.save
-      position = 1
-      params[:item_ordering].each do |id|
-        item = Item.find(id)
-        item.position = position
-        item.save
-        position += 1
-      end
-
-      if @old_lane.id != @lane.id
-        position = 1
-        @old_lane.items.each do |item|
-          item.position = position
-          item.save
-          position += 1
-        end
-      end
-
+      handle_item_drop
       hobo_ajax_response
       return
     end
@@ -40,33 +19,36 @@ class ItemsController < ApplicationController
 
   def edit
     if request.xhr?
-      @item = Item.find(params[:item_id])
-      @old_lane = @item.lane
-      @lane = Lane.find(params[:lane_id])
-      @item.lane = @lane
-      @item.save
-      position = 1
-      params[:item_ordering].each do |id|
-        item = Item.find(id)
-        item.position = position
-        item.save
-        position += 1
-      end
-
-      if @old_lane.id != @lane.id
-        position = 1
-        @old_lane.items.each do |item|
-          item.position = position
-          item.save
-          position += 1
-        end
-      end
-
+      handle_item_drop
       hobo_ajax_response
       return
     end
 
     @item = find_instance
+    # hobo_update
   end
 
+  def handle_item_drop
+    item = Item.find(params[:item_id])
+    old_lane = item.lane
+    lane = Lane.find(params[:lane_id])
+    item.lane = lane
+    item.save
+    position = 1
+    params[:item_ordering].each do |id|
+      item = Item.find(id)
+      item.position = position
+      item.save
+      position += 1
+    end
+
+    if old_lane.id != lane.id
+      position = 1
+      old_lane.items.each do |item|
+        item.position = position
+        item.save
+        position += 1
+      end
+    end
+  end
 end
