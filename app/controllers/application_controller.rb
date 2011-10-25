41f45c55b15fc72fccd5d9b646ae2994c38f748b
Richard Dale <richard.dale@codethink.co.uk>
2011-10-25 09:31:18 +0100

    * Move the handle_item_drop() method to the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,29 @@ class ApplicationController < ActionController::Base
 :do_reset_password] do
      login_required unless User.count == 0
   end
+
+  def handle_item_drop
+    item = Item.find(params[:item_id])
+    old_lane = item.lane
+    lane = Lane.find(params[:lane_id])
+    item.lane = lane
+    item.save
+    position = 1
+    params[:item_ordering].each do |id|
+      item = Item.find(id)
+      item.position = position
+      item.save
+      position += 1
+    end
+
+    if old_lane.id != lane.id
+      position = 1
+      old_lane.items.each do |item|
+        item.position = position
+        item.save
+        position += 1
+      end
+    end
+  end
+
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,28 +7,7 @@ class ProjectsController < ApplicationController
 
   def show
     if request.xhr?
-      @item = Item.find(params[:item_id])
-      @old_lane = @item.lane
-      @lane = Lane.find(params[:lane_id])
-      @item.lane = @lane
-      @item.save
-      position = 1
-      params[:item_ordering].each do |id|
-        item = Item.find(id)
-        item.position = position
-        item.save
-        position += 1
-      end
-
-      if @old_lane.id != @lane.id
-        position = 1
-        @old_lane.items.each do |item|
-          item.position = position
-          item.save
-          position += 1
-        end
-      end
-
+      handle_item_drop
       hobo_ajax_response
       return
     end
