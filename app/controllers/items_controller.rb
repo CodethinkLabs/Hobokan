2180698039686e50e6fe92d9610661bb232ddb56
Richard Dale <richard.dale@codethink.co.uk>
2011-10-26 09:29:50 +0100

    * Don't show lanes and items on the main navigation bar

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all
+  auto_actions :all, :except => :index
   auto_actions_for :lane, [:new, :create]
 
   def new_for_lane
