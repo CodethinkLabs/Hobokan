730ab77ad730af8d8e02c0d93c137ecf4378e833
Richard Dale <richard.dale@codethink.co.uk>
2011-11-02 10:03:57 +0000

    * Revert the action mailer content type change in case it is what caused the staging app to fail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => "no-reply@#{host}",
-          :content_type => "text/html"
+  default :from => "no-reply@#{host}"
 
   def forgot_password(user, key)
     @user, @key = user, key
