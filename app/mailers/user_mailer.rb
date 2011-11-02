ea447503da6f53a79127da6c28b1617ca8b9191a
Richard Dale <richard.dale@codethink.co.uk>
2011-11-02 09:55:19 +0000

    * Configure the mailer to user "text/html" as a content type

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ActionMailer::Base
-  default :from => "no-reply@#{host}"
+  default :from => "no-reply@#{host}",
+          :content_type => "text/html"
 
   def forgot_password(user, key)
     @user, @key = user, key
