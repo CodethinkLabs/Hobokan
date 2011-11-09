95b571a660de16f52bf90879908bc13775216010
Richard Dale <richard.dale@codethink.co.uk>
2011-11-09 16:53:16 +0000

    * Add a down migration for the lane position reordering for completeness

--- db/migrate/20111109094032_reorder_lanes.rb
+++ db/migrate/20111109094032_reorder_lanes.rb
@@ -21,5 +21,18 @@ class ReorderLanes < ActiveRecord::Migration
   end
 
   def self.down
+    # ActiveRecord::Base.logger = Logger.new(STDERR)
+    Project.all.each do |project|
+      project.lanes.each do |lane|
+        length = lane.items.length
+        lane.items.each do |item|
+          item.skip_version do
+            # puts "length: #{length} #{item.inspect}"
+            item.position = (length - item.position) + 1
+            item.save
+          end
+        end
+      end
+    end
   end
 end
