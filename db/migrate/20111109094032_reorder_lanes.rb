573ba96168d4e58b2057c1d95a51c1b9fe033f10
Richard Dale <richard.dale@codethink.co.uk>
2011-11-09 10:18:59 +0000

    * Remove debug logging

--- db/migrate/20111109094032_reorder_lanes.rb
+++ db/migrate/20111109094032_reorder_lanes.rb
@@ -5,13 +5,13 @@ end
 
 class ReorderLanes < ActiveRecord::Migration
   def self.up
-    ActiveRecord::Base.logger = Logger.new(STDERR)
+    # ActiveRecord::Base.logger = Logger.new(STDERR)
     Project.all.each do |project|
       project.lanes.each do |lane|
         length = lane.items.length
         lane.items.each do |item|
           item.skip_version do
-            puts "length: #{length} #{item.inspect}"
+            # puts "length: #{length} #{item.inspect}"
             item.position = (length - item.position) + 1
             item.save
           end
