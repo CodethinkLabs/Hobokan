df84006606b313e3879f95d3288cdac983f106d1
Richard Dale <richard.dale@codethink.co.uk>
2011-11-12 08:48:10 +0000

    * If the color of a lane is blank set it to #000000

--- db/migrate/20111111104246_add_default_lane_color.rb
+++ db/migrate/20111111104246_add_default_lane_color.rb
@@ -5,6 +5,14 @@ class AddDefaultLaneColor < ActiveRecord::Migration
     drop_table :history_entries
 
     change_column :lanes, :color, :string, :limit => 255, :default => "#000000"
+
+    # ActiveRecord::Base.logger = Logger.new(STDERR)
+    Lane.all.each do |lane|
+      if lane.color.blank?
+        lane.color = '#000000'
+        lane.save
+      end
+    end
   end
 
   def self.down
