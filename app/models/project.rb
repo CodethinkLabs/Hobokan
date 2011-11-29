c9d1227958f86ccf4e6e3b01d302f959259d1fa9
Paul Sherwood <paul.sherwood@codethink.co.uk>
2011-11-29 20:58:54 +0000

    change log for item and project now use same simpler code

--- app/models/project.rb
+++ app/models/project.rb
@@ -40,64 +40,10 @@ class Project < ActiveRecord::Base
     return result.join("\n")
   end
 
-  class Change
-    attr_accessor :user, :comment, :date
+  def versions
+    return VestalVersions::Version.where(:versioned_id => items).order("created_at DESC").limit(50)
   end
 
-  def changes
-    members = project_members
-    member_ids = members.map {|m| m.id }
-
-    user_names = {}
-    members.each do |member|
-      # logger.debug("member: #{member.inspect} #{member.user.inspect}")
-      # logger.debug(user_names)
-      user_names[member.id] = member.user.name
-    end
-
-    versions = VestalVersions::Version.
-                  where(:user_id => member_ids).
-                  where(:user_type => 'ProjectMember').
-                  where("created_at > ?", Date.today - 7).
-                  order("created_at DESC")
-    result = []
-    versions.each do |v|
-      change = Change.new
-
-      if user_names.has_key?(v.user_id)
-        change.user = user_names[v.user_id]
-      else
-        change.user = 'anon'
-      end
-
-      change.date = v.created_at.strftime("%d/%m/%Y")
-      change.comment = ""
-
-      if v.modifications['title']
-        change.comment += "Renamed S#{v.versioned_id} '#{v.modifications['title'][0]}' as '#{v.modifications['title'][1]}'"
-      end
-
-      if v.modifications['text']
-        change.comment += v.modifications['text'][1].to_s
-      end
-
-#      if v.modifications['position']
-#        title = Item.find(v.versioned_id).title
-#        change.comment += "'#{title}' position changed from #{v.modifications['position'][0]} to #{v.modifications['position'][1]} "
-#      end
-
-      if v.modifications['lane_id']
-        title = Item.find(v.versioned_id).title
-        change.comment += "Moved '#{title}' from #{Lane.find(v.modifications['lane_id'][0]).title} to #{Lane.find(v.modifications['lane_id'][1]).title} "
-      end
-
-      if change.comment != ""
-        result << change
-      end
-    end
-
-    return result
-  end
 
   # --- Permissions --- #
 
