0beafd23d3fe268163d9b9991b9e63bb80cc6026
Paul Sherwood <paul.sherwood@codethink.co.uk>
2011-12-04 16:12:45 +0000

    autogenerated files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111204141218) do
+ActiveRecord::Schema.define(:version => 20111204154956) do
 
   create_table "checklists", :force => true do |t|
     t.string    "text"
