5e161798486ebcd15fcc6024612c787c17833068
Richard Dale <richard.dale@codethink.co.uk>
2011-12-12 20:39:15 +0000

    Initialize the item_assignments project_member_id column

--- db/migrate/20111209155815_item_assignment_project_member.rb
+++ db/migrate/20111209155815_item_assignment_project_member.rb
@@ -1,10 +1,26 @@
+class ActiveRecord::Base
+  def raw_update(query)
+    self.class.connection.execute(query)
+  end
+end
+
 class ItemAssignmentProjectMember < ActiveRecord::Migration
   def self.up
+    temp = {}
+    ItemAssignment.all.each do |assignment|
+      member = ProjectMember.where(:user_id => assignment.user_id).where(:project_id =>assignment.item.project)
+      temp[assignment.id] = member[0].id
+    end
+
     add_column :item_assignments, :project_member_id, :integer
     remove_column :item_assignments, :user_id
 
     remove_index :item_assignments, :name => :index_item_assignments_on_user_id rescue ActiveRecord::StatementInvalid
     add_index :item_assignments, [:project_member_id]
+
+    ItemAssignment.all.each do |assignment|
+      assignment.raw_update("UPDATE item_assignments SET project_member_id = #{temp[assignment.id]} where id = #{assignment.id}")
+    end
   end
 
   def self.down
