3e9154270c5d55aade67b0784fd0a306450872d7
Richard Dale <richard.dale@codethink.co.uk>
2011-12-15 20:30:22 +0000

    Remove obsolete table references which should have been dropped long before this migration is run

--- db/migrate/20111215193009_add_milestones.rb
+++ db/migrate/20111215193009_add_milestones.rb
@@ -1,9 +1,5 @@
 class AddMilestones < ActiveRecord::Migration
   def self.up
-    drop_table :checklist_items
-    drop_table :checklists
-    drop_table :item_project_members
-
     create_table :milestones do |t|
       t.string   :name
       t.date     :date
@@ -26,36 +22,6 @@ class AddMilestones < ActiveRecord::Migration
     remove_column :items, :milestone_id
 
     change_column :project_members, :administrator, :boolean, :default => false
-
-    create_table "checklist_items", :force => true do |t|
-      t.string    "text"
-      t.boolean   "done"
-      t.timestamp "created_at"
-      t.timestamp "updated_at"
-      t.integer   "item_id"
-    end
-
-    add_index "checklist_items", ["item_id"], :name => "index_checklist_items_on_item_id"
-
-    create_table "checklists", :force => true do |t|
-      t.string    "text"
-      t.boolean   "done"
-      t.timestamp "created_at"
-      t.timestamp "updated_at"
-      t.integer   "item_id"
-    end
-
-    add_index "checklists", ["item_id"], :name => "index_checklists_on_item_id"
-
-    create_table "item_project_members", :force => true do |t|
-      t.timestamp "created_at"
-      t.timestamp "updated_at"
-      t.integer   "project_member_id"
-      t.integer   "item_id"
-    end
-
-    add_index "item_project_members", ["item_id"], :name => "index_item_project_members_on_item_id"
-    add_index "item_project_members", ["project_member_id"], :name => "index_item_project_members_on_project_member_id"
 
     drop_table :milestones
 
