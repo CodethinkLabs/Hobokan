c1c9360d7ba88c74b6cf5e4e498f93afb8c2c1d6
Paul Sherwood <paulsherwood@codethink.co.uk>
2012-01-20 12:44:10 +0000

    invited users need to see themselves

--- app/models/user.rb
+++ app/models/user.rb
@@ -106,6 +106,6 @@ class User < ActiveRecord::Base
   end
 
   def view_permitted?(field)
-    acting_user == self || acting_user.administrator?
+    acting_user == self || acting_user.administrator? || self.state == "invited"
   end
 end
