d9b8951c5c4a9b5d873657e9731f66c70fb58426
Jonathan Maw <jonathan.maw@codethink.co.uk>
2012-03-28 16:41:31 +0100

    Added public_viewable to the project model and edit view.

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,7 @@ class Project < ActiveRecord::Base
   fields do
     name :string
     details :markdown
-    publicview :boolean, :default => false
+    public_viewable :boolean, :default => false
     timestamps
   end
 
