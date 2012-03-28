0eb5a5c6a1be017d14148b16be65a60af8fb5eb9
Francisco Marchena <franciscomarchena@codethink.co.uk>
2012-03-28 10:52:27 +0100

    changes in lane_test.rb, project_test.rb and test_helper.rb. lane_test.rb has the test for its functions and permissions create. project_test.rb and test_helper.rb has the first attempt for the last week.

--- test/unit/lane_test.rb
+++ test/unit/lane_test.rb
@@ -1,8 +1,166 @@
 require 'test_helper'
 
 class LaneTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  setup do
+    Lane.delete_all
+    Project.delete_all
+    Milestone.delete_all
+    ProjectMember.delete_all
+    # Create one Admin.
+    @oneAdmin = User.create( :name => "oneAdmin name",
+                             :email_address => "oneAdmin@oneAdmin.com",
+                             :administrator => true,
+                             :role => :manager )
+    # Create one User.
+    @oneUser = User.create( :name => "oneUser name",
+                            :email_address => "oneUser@oneUser.com",
+                            :administrator => true,
+                            :role => :manager )
+    
+    # Create one Project.
+    @oneProject = Project.create( :name => "Project Unit Test",
+                                  :details => "project for lane unit testing" )
+
+    assert_not_nil(@oneProject, "Expected created project was not nil")
+    
+    # Create a Lane associated to the project.      
+    @oneLane = @oneProject.lanes.create( :title => "Lane Unit Test", 
+                                         :position => -1, 
+                                         :background_color => "black", 
+                                         :color => '#FFFFFF',
+                                         :todo => false,
+                                         :project => @oneProject )
+    assert_not_nil(@oneLane, "Expected created lane was not nil")
+    
+    # Check that is the unique lane create into db.
+    assert_equal(1, Lane.count, "Expected lane created was unique")
+    
+    # Check that the unique lane created is our lane
+    @secondLane = Lane.find(:first, :conditions => ["color = '#FFFFFF'"])
+    assert_equal(@secondLane.title, "Lane Unit Test", 
+                 "Expected lane in db was the lane created")
+                 
+    # Create Milestones associated to the project
+    @oneMilestone = @oneProject.milestones.create(
+                                   :name => "first milestone",
+                                   :description => 
+                                         "first milestone for lane unit test",
+                                   :project => @oneProject )
+    assert_not_nil(@oneMilestone, "Expected milestone was created")
+    
+    @twoMilestone = @oneProject.milestones.create(
+                                  :name => "second milestone",
+                                  :description => 
+                                          "second milestone for lane unit test",
+                                  :project => @oneProject )
+    assert_not_nil(@twoMilestone, "Expected milestone was created")
+      
   end
+	
+  # Test for function name.
+  test "Check function name" do
+    nameOne = @oneLane.name
+    assert_equal(nameOne, @oneLane.title, "Expected name is equal to title")
+  end
+  
+  # Test for the function milestones.
+  test "Check that function 'milestones' return the milestones of the project 
+        associated to a lane" do
+        
+    retMilestones = @oneLane.project.milestones
+    assert_equal(2, retMilestones.size,
+                "Expected two milestones associated to the project which is 
+                 associated to the lane")
+  end
+  
+  # Test for permissions.
+    # Test for create.
+      # Check creation without project_members (new project)
+      test "Check create permission without project_members" do
+        # Create a project
+        notProjectMem = Project.create(:name => "without project Members")
+        
+        # Check that there are not any project member
+        assert_equal(0, ProjectMember.count)
+        
+        # Create one lane
+        notProjectMem_Lane = notProjectMem.lanes.create(
+                                           :title => "without project Members",
+                                           :project => notProjectMem)
+        
+        assert(notProjectMem_Lane.creatable_by?(@oneUser), 
+          "Expected @oneUser can create one lane at beginning of the project.")
+      end
+      
+      # Check creation with project_members being admin of the project
+      test "Check create permission with project_members" do
+        # Create a project.
+        aProject = Project.create!( :name => "Testing permsision create",
+                                    :details => "project for lane permission" )
+        aProject2 = Project.create!( :name => "Testing permsision create",
+                                     :details => "idem for project 2" )
+        # Create an admin.
+        aAdmin = User.create!( :name => "oneUser name",
+                               :email_address => "oneUser@oneUser.com",
+                               :password => "password",
+                               :administrator => true,
+                               :role => :manager )
+        aUser = User.create!(  :name => "twoUser name",
+                               :email_address => "twoUser@twoUser.com",
+                               :password => "password",
+                               :administrator => false,
+                               :role => :manager )
+        # Associated the admin to the project --> membership
+        aMembership = ProjectMember.create!( :project => aProject,
+                                             :user => aAdmin )
+        aMembership2 = ProjectMember.create!( :project => aProject2,
+                                              :user => aUser,
+                                              :administrator => false)
+        # Check that there are two project members
+        assert_equal(2, ProjectMember.count)
+        
+        # is the admin able to create a lane in the project?
+        ProjectMember.memberships = []
+        ProjectMember.admin_memberships = []
+        ProjectMember.view_memberships = []
+
+        members = ProjectMember.where(:user_id => aAdmin)
+        
+        puts members.size
+        puts members[0].name
+        puts Project.count
+        
+        members.each do |member|
+          ProjectMember.memberships.push(member.project_id)
+          ProjectMember.admin_memberships.push(member.project_id) unless !member.administrator
+          ProjectMember.view_memberships.push(member.project_id)
+        end
+        
+        puts "Lane#create_permitted? #{ProjectMember.admin_memberships.inspect} project_id: #{members[0].project_id}"
+        puts "Lane#create_permitted? #{ProjectMember.memberships.inspect} project_id: #{members[0].project_id}"
+        puts "Lane#create_permitted? #{ProjectMember.view_memberships.inspect} project_id: #{members[0].project_id}"
+        # Check if the project has or not admin_memberships associated
+         # aProject has an admin_membership because admin created it
+         # aProject2 does not have an admin_membership because aUser created it
+        
+        assert(ProjectMember.admin_memberships.include?(aProject.id), "Expected that the project includes admin memberships")
+        
+        # Check that any user cannot create one lane in aProject2
+        assert_equal(1, aProject2.project_members.count, 
+                     "Expected one project member associated." )
+        assert(!ProjectMember.admin_memberships.include?(aProject2.id), 
+               "Expected that the project doesn't include admin memberships" )
+         
+        assert(!aProject2.lanes.create!(:title => "no lane created").creatable_by?(aAdmin),
+               "Expected create fail because there aren't any admin_memberships")
+        
+      end
+
+      # Check creation with project_members not being admin of the project
+    # Test for update.
+    # Test for destroy.
+    # Test for view.
+
+	
 end
--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,8 +1,246 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  # Test creation and initialization by default.
+  test "Check creation an initialization Project by default" do
+    # Creating a new default lane.
+    default_project = Project.new
+    default_project.save
+    assert_not_nil(default_project, "Expected that project is not null")
+		
+    # Test that default values were used.
+    assert_equal(nil, default_project.name)
+    assert_equal(nil, default_project.details)
+    assert_equal("running", default_project.state)
+    
+    # Test that it is possible to delete the project.
+    assert default_project.destroy
+    assert_nil(default_project,"Expected that project was destroyed")
   end
-end
+
+  # Setup a Project to use in this test. 
+  setup do
+    Project.delete_all #delete previous Projects
+    @oneProjectUnit = Project.create( :name    => "Project Unit Test",
+                                      :details => "Test creates in Unit Test")
+    assert_not_nil(@oneProjectUnit)
+    
+    # Check that is the unique project create into db.
+    assert_equal(1, Project.count)
+  end
+  
+  # Test creation and initialization with values.
+  test "Check creation an initialization Project with values" do
+    assert_not_nil(@oneProjectUnit, "project is not null")
+    
+    # test that default values were used
+    assert_equal("Project Unit Test",@oneProjectUnit.name)
+    assert_equal("Test creates in Unit Test",@oneProjectUnit.details)
+    assert_equal("running",@oneProjectUnit.state) # value by default, modify
+                                                  # by lifecycle.
+    assert_not_nil(@oneProjectUnit.created_at) # check timestamp.
+    assert_not_nil(@oneProjectUnit.updated_at) # check timestamp.
+    assert_not_nil(@oneProjectUnit.key_timestamp,
+                   "Expected that key_timestamp was not null") # NULL??
+  end
+
+  # Test Update.
+  test "Check updating Project with values" do
+    @oneProjectUnit.update_attributes( :name    => "Updated name",
+                                       :details => "Updating project in U.T.",
+                                       :state => "update state" )
+    assert_equal("Updated name", @oneProjectUnit.name)
+    assert_equal("Updating project in U.T.", @oneProjectUnit.details)
+    assert_not_equal("update state", @oneProjectUnit.state)
+    assert_not_nil(@oneProjectUnit.created_at) # check timestamp.
+    assert_not_nil(@oneProjectUnit.updated_at) # check timestamp.
+    assert_not_nil(@oneProjectUnit.key_timestamp) # NULL??
+    
+    # Test update a field and save.
+    @oneProjectUnit.name = "Updated name once again"
+    assert @oneProjectUnit.save
+    assert_equal("Updated name once again", @oneProjectUnit.name)
+  end
+  
+	
+  # Test that the fields of project can be Readables/Writables. 
+ 	# Note*: write does not invoke the model based validation !!
+  test "Check that every field from project can be read/write" do
+    
+    assert_nothing_raised { @oneProjectUnit.name } # not expect any exception.
+    assert_nothing_raised { @oneProjectUnit.name = "write name" }  
+    
+    assert_nothing_raised { @oneProjectUnit.details } 
+    assert_nothing_raised { @oneProjectUnit.details = "write details" } 
+    
+    assert_nothing_raised { @oneProjectUnit.state } 
+    assert_nothing_raised { @oneProjectUnit.state = "write state" } 
+    assert_equal("write state", @oneProjectUnit.state) # it's work, see Note*
+    
+    assert_nothing_raised { @oneProjectUnit.created_at } 
+    assert_nothing_raised { @oneProjectUnit.created_at = 0 } 
+    
+    assert_nothing_raised { @oneProjectUnit.updated_at } 
+    assert_nothing_raised { @oneProjectUnit.updated_at = 0 } 
+    
+    assert_nothing_raised { @oneProjectUnit.key_timestamp } 
+    assert_nothing_raised { @oneProjectUnit.key_timestamp = 0 } 
+            
+    flunk ("Write does not invoke the model validation !!")
+    
+  end
+  
+  # Test the name validation. 
+  test "Check that name is shorter than 50 characters" do
+    assert_required_length_less_than(@oneProjectUnit, :name, 50)
+  end
+	
+  test "Check that name is longer than 4 characters" do
+    assert_required_length_more_than(@oneProjectUnit, :name, 4)
+  end
+  
+  # Test Project has many :items.
+  test "Check that Project has many items" do
+    # Check through class definition
+    reflection_items = @oneProjectUnit.class.reflect_on_association(:items)
+    assert_not_nil(reflection_items, "Project is not associated to items")
+    assert_equal(reflection_items.macro, :has_many, 
+                "Project doesn't have any items")
+                
+    # Check through creation and association lanes to one project
+    projectHasItems = Project.create(:name => "projectHasItems")
+    projectHasItems.save
+    assert_not_nil(projectHasItems)
+    assert_empty projectHasItems.items
+    
+    itemProject1 = projectHasItems.items.build( :title => "item1inProject",
+                                                :project_id => 
+                                                           projectHasItems.id )
+    assert_not_nil(itemProject1)
+    
+    itemProject2 = projectHasItems.items.build( :title => "item2inProject",
+                                                :project_id => 
+                                                           projectHasItems.id )
+    assert_not_nil(itemProject2)
+    assert_equal(2, projectHasItems.items.size)
+    
+  end
+  
+  # Test Project has many :lanes.
+  test "Check that Project has many lanes" do
+    reflection_lanes = @oneProjectUnit.class.reflect_on_association(:lanes)
+    assert_not_nil(reflection_lanes, "Project is not associated to lanes")
+    assert_equal(reflection_lanes.macro, :has_many, 
+                 "Project doesn't have any lanes")
+    
+    # Check through creation and association lanes to one project
+    projectHasLanes = Project.create(:name => "projectHasLanes")
+    projectHasLanes.save
+    assert_not_nil(projectHasLanes)
+    assert_empty(projectHasLanes.lanes)
+    
+    laneProject1 = projectHasLanes.lanes.build( :title => "lane1inProject", 
+  	                                           :project_id => 
+  	                                                      projectHasLanes.id )
+    laneProject1.save
+    assert_not_nil(laneProject1)
+    
+    laneProject2 = projectHasLanes.lanes.build( :title => "lane2inProject", 
+                                                :project_id => 
+                                                           projectHasLanes.id )
+    laneProject2.save
+    assert_not_nil(laneProject2)
+    
+    assert_equal(2, projectHasLanes.lanes.size)
+    
+  end
+  
+  # Test Project has many :project_members.
+  test "Check that Project has many project_members" do
+    reflection_project_members = 
+                 @oneProjectUnit.class.reflect_on_association(:project_members)
+    assert_not_nil(reflection_project_members, 
+                   "Project is not associated to items")
+    assert_equal(reflection_project_members.macro, :has_many, 
+                "Project doesn't have any project_members")
+  end
+  
+  # Test Project has many :users.
+  test "Check that Project has many users" do
+    # Check through class definition
+    reflection_users = @oneProjectUnit.class.reflect_on_association(:users)
+    assert_not_nil(reflection_users, "Project is not associated to users")
+    assert_equal(reflection_users.macro, :has_many, 
+                "Project doesn't have any users")
+  end
+  
+  # Check through creation and association users to one project 
+  # (project members)
+  test "Check that one Project could have many users (project members)" do
+  	
+  	admin = User.create(:name => "adminuser",
+  	                    :administrator => true)
+  	admin.save
+  	assert_not_nil(admin,"Administrator create and save")
+  	
+  	not_admin = User.create(:name => "not_admin",
+  	                        :administrator => false)
+  	not_admin.save
+	assert_not_nil(not_admin,"Non administrator create and save")
+	
+  	project_admin = admin.projects.build(:name => "admin_project")
+  	project_admin.save
+  	assert_not_nil(project_admin)
+  	
+  	member_admin = admin.project_members.build( :administrator => true,
+  	                                            :project_id => 
+  	                                                         project_admin.id,
+  	                                            :user_id => admin.id)
+  	member_admin.save
+  	assert member_admin
+  	
+  	member_not_admin = admin.project_members.build( :administrator => true,
+  	                                                :project_id => 
+  	                                                         project_admin.id,
+  	                                                :user_id => not_admin.id )
+  	member_not_admin.save
+  	assert member_not_admin
+	
+  	assert_equal(2, project_admin.project_members.size)
+  	
+  end
+  
+  # Test Project has many :milestones
+  test "Check that Project has many milestones" do
+  
+    # Check through class definition
+    reflection_milestones = 
+                      @oneProjectUnit.class.reflect_on_association(:milestones)
+    assert_not_nil(reflection_milestones, 
+                   "Project is not associated to milestones")
+    assert_equal(reflection_milestones.macro, :has_many, 
+                "Project doesn't have any milestones")
+                
+    # Check through creation and association milestones to one project
+    projectHasMilestones = Project.create(:name => "proMilUnit")
+    projectHasMilestones.save
+    assert_not_nil(projectHasMilestones)
+    assert_empty projectHasMilestones.milestones
+    
+    milestoneProject1 = projectHasMilestones.milestones.create( 
+                                                     :name => "mileston1inPro", 
+  	                                                :project_id => 
+  	                                                 projectHasMilestones.id )
+    assert_not_nil(milestoneProject1)
+    milestoneProject2 = projectHasMilestones.milestones.create( 
+                                                :name => "milestone2inPro", 
+                                                :project_id => 
+                                                      projectHasMilestones.id )
+    assert_not_nil(milestoneProject2)
+    assert_equal(2, projectHasMilestones.milestones.size)
+    
+  end
+  
+
+ end
