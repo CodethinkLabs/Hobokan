3623ae8633af355b2ef90f655e57e6ecff13ba60
Richard Dale <richard.dale@codethink.co.uk>
2012-04-10 13:15:19 +0100

    Now that the kanban staging app has a SECRET_TOKEN environment variable, change the config to use it

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# Hobokan::Application.config.secret_token = ENV['SECRET_TOKEN']
-Hobokan::Application.config.secret_token = "935daad11fcbda3e84f9ea053d0c7ae3190d17965597e31e921eb39969cd2e4214b1a1f3fd2d6664891a17c6c0346f0789268ab2f0ab9d292a671b7563b90be6"
+Hobokan::Application.config.secret_token = ENV['SECRET_TOKEN']
