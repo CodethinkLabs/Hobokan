60ae34e403579eb1fe9f4bcfa0875d28b345ce03
Richard Dale <richard.dale@codethink.co.uk>
2012-04-26 16:21:32 +0100

    Update the schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120218074210) do
+ActiveRecord::Schema.define(:version => 20120426080719) do
 
   create_table "comments", :force => true do |t|
     t.text      "detail"
