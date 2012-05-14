86f3a723bd543213fe2342413c58f2099ae771fb
Richard Dale <richard.dale@codethink.co.uk>
2012-05-14 12:31:05 +0100

    Move the drag and drop function into its own javascript file so that it won't be sent to the browser for guest users

--- public/javascripts/kanban.js
+++ public/javascripts/kanban.js
@@ -5,33 +5,6 @@ var count_cards = function () {
   });
 }
 
-var drop_handler = function() {
-  // Get the length of the lane, excluding the 'h3' heading
-  var lane_length = jQuery(this).parent().children().length - 1;
-  // The positions of the item rows in the database are from 1 to
-  // the lane length in descending order, whereas in the kanban
-  // board they start at 0 and go in ascending order.
-  var item_position = lane_length - this.attr("data-itemidx");
-  var item_id = this.attr("id").substr(1);
-  var lane_id = this.parent().attr("id").substr(1);
-
-  var startchar = "?"
-  if (window.location.href.indexOf("?") != -1) startchar = "&"
-
-  Hobo.ajaxRequest( window.location.href + startchar +
-                    "lane_id=" + lane_id +
-                    "&item_id=" + item_id +
-                    "&item_position=" + item_position,
-                    [],
-                    { params: { lane_id: lane_id, item_id: item_id },
-                      action: 'show',
-                      controller: 'projects',
-                      method: 'get',
-                      message: "Please wait"
-                    } );
-  count_cards ();
-}
-
 /*
  * Add a box for a new task onto its lane, and then
  * close the new task dialog
--- public/javascripts/kanban_dragdrop.js
+++ public/javascripts/kanban_dragdrop.js
@@ -1,16 +1,21 @@
+
 var drop_handler = function() {
-
-  var item_position = "";
-
-  item_position = "&item_position=" + jQuery(this).parent().children().filter('li').index(this)
-
+  // Get the length of the lane, excluding the 'h3' heading
+  var lane_length = jQuery(this).parent().children().length - 1;
+  // The positions of the item rows in the database are from 1 to
+  // the lane length in descending order, whereas in the kanban
+  // board they start at 0 and go in ascending order.
+  var item_position = lane_length - this.attr("data-itemidx");
   var item_id = this.attr("id").substr(1);
   var lane_id = this.parent().attr("id").substr(1);
 
   var startchar = "?"
   if (window.location.href.indexOf("?") != -1) startchar = "&"
 
-  Hobo.ajaxRequest( window.location.href + startchar + "lane_id=" + lane_id + "&item_id=" + item_id + item_position,
+  Hobo.ajaxRequest( window.location.href + startchar +
+                    "lane_id=" + lane_id +
+                    "&item_id=" + item_id +
+                    "&item_position=" + item_position,
                     [],
                     { params: { lane_id: lane_id, item_id: item_id },
                       action: 'show',
