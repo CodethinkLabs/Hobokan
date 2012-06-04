f155ce19d9514dba68d127967ed5c0221152c000
Paul Sherwood <paul.sherwood@codethink.co.uk>
2012-06-04 21:24:41 +0100

    missed file updates from 1.4 creation

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,20 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Hobokan
   class Application < Rails::Application
-
-  config.hobo.dryml_only_templates = true
-  config.hobo.app_name = "kanban"
+    config.hobo.dryml_only_templates = true
+    config.hobo.app_name = "kanban"
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w(admin.css admin.js)
+    config.assets.precompile += %w(front.css front.js)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
