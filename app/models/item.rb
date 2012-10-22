6e743792545577a5701c38b5690342cdde57a9c5
Paul Sherwood <paulsherwood@codethink.co.uk>
2012-10-22 08:12:36 +0100

    show age of task in current lane

--- app/models/item.rb
+++ app/models/item.rb
@@ -71,9 +71,7 @@ class Item < ActiveRecord::Base
     if self.lane == nil
       self.lane = project.lanes.visible[0]
     end
-    if self.lane.closed
-      self.end_date = Date.today
-    end
+    self.end_date = Date.today
   end
 
   def enqueue_item
